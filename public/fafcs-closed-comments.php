c5097b6196f88a94b874158b8d1c8afbef34563c
Ariful Islam <46777189+ArifulIslam124@users.noreply.github.com>
2020-04-06 01:14:12 +0300

    Add files via upload

---
+++ public/fafcs-closed-comments.php
@@ -0,0 +1,19 @@
+<?php
+
+// If this file is called directly, abort.
+defined( 'WPINC' ) or die( 'Damn it.! Dude you are looking for what?' );
+
+/**
+ * Closed comments template
+ *
+ * @category   HTML
+ * @package    FAFCS
+ * @subpackage Public
+ * @author     The Future Aim IT Solutions <info@ateam.host>
+ * @license    http://www.gnu.org/licenses/ GNU General Public License
+ * @link       https://github.com/ArifulIslam124/Future-Aim-Social-Comment-system
+ *
+ *
+ * @note Add html if you want to modify the closed comments page
+ */
+
